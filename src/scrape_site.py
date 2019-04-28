3bd93eef306682a621f48fca95d1d6fbee012d1a
Demond Love <dhlove1s@gmail.com>
2019-04-28 18:13:02 -0500

    Add database insert to the scrape_site.py script

--- src/scrape_site.py
+++ src/scrape_site.py
@@ -3,8 +3,10 @@
 if __name__ == '__main__':
     import sys
     import os
+    import pymysql
     import inspect
     import pandas as pd
+    import sqlalchemy as sql
 
     filename = inspect.getframeinfo(inspect.currentframe()).filename
     curpath = os.path.dirname(os.path.abspath(filename))
@@ -46,3 +48,35 @@ if __name__ == '__main__':
         )
         
     df.to_csv(curpath + '/data/BoxOfficeMojoDataset.csv',index=False)
+
+    # Create connection variables
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASS = os.environ.get('DB_PASS')
+    DB_HOST = 'localhost'
+    DB_PORT = 3306
+    DATABASE = 'movies'
+
+    # Connect to the database for pymysql connection
+    connection = pymysql.connect(host=DB_HOST,
+                                 user=os.environ.get('DB_USER'),
+                                 password=os.environ.get('DB_PASS'),
+                                 db=DATABASE,
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+
+    # Create connection string for sqlalchemy
+    connect_string = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(DB_USER, DB_PASS, DB_HOST, DB_PORT, DATABASE)
+
+    # To setup the persistent connection, you do the following:
+    sql_engine = sql.create_engine(connect_string)
+    
+    # Ensue the features are in the appropriate order to be inserted into the database
+    df[['id', 'Title', 'Distributor', 'Genre', 'MPAARating', 'ProductionBudget', 'ReleaseDate', 'Runtime', 'DomesticGross', 'ForeignGross', 'WorldwideGross', 'OpeningWeekendGross', 'OpeningWeekendTheaters', 'WidestTheaters', 'Genres', 'URL']]
+
+    for i in range(len(df)):
+    with connection.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `BoxOfficeMojo` (`id`, `Title`, `Distributor`, `Genre`, `MPAARating`, `ProductionBudget`, `ReleaseDate`, `Runtime`, `DomesticGross`, `ForeignGross`, `WorldwideGross`, `OpeningWeekendGross`, `OpeningWeekendTheaters`, `WidestTheaters`, `Genres`, `URL`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        cursor.execute(sql, (str(df.iloc[i][0]), str(df.iloc[i][1]), str(df.iloc[i][2]), str(df.iloc[i][3]), str(df.iloc[i][4]), str(df.iloc[i][5]), str(df.iloc[i][6]), str(df.iloc[i][7]), str(df.iloc[i][8]), str(df.iloc[i][9]), str(df.iloc[i][10]), str(df.iloc[i][11]), str(df.iloc[i][12]), str(df.iloc[i][13]), str(df.iloc[i][14]), str(df.iloc[i][15])))
+    # connection is not autocommit by default. So you must commit to save your changes.
+    connection.commit()
